16e87b1228a51f8b688c0cfd60827c0394f90db5
Konstantin Gazha <konstgazha@yandex.ru>
2018-03-02 13:47:24 +0500

    Create field based permission

--- alphabi_rest_service/core/permissions.py
+++ alphabi_rest_service/core/permissions.py
@@ -5,9 +5,7 @@ from django.contrib.contenttypes.models import ContentType
 
 class GeneralPermission(BasePermission):
     def has_permission(self, request, view):
-        content_type = ContentType.objects.get_for_model(view.model)
-        all_permissions = Permission.objects.filter(content_type=content_type)
-        for permission in all_permissions:
-            if request.user.has_perm("core." + permission.codename):
-                return True
-        return False
+        if view.get_user_permissions():
+            return True
+        else:
+            return False
--- alphabi_rest_service/core/views.py
+++ alphabi_rest_service/core/views.py
@@ -3,6 +3,7 @@ from core.permissions import GeneralPermission
 from rest_framework.viewsets import ModelViewSet
 from django.apps import apps
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
 
 
 class GeneralViewSet(ModelViewSet):
@@ -13,13 +14,29 @@ class GeneralViewSet(ModelViewSet):
         return super(GeneralViewSet, self).dispatch(request, *args, **kwargs)
 
     def get_model(self):
-        model_name = self.kwargs.get('model')
+        model_name = self.kwargs.get('model').lower()
         return apps.get_model(ContentType.objects.get(model=model_name).app_label,
                               model_name)
 
     def get_queryset(self):
         return self.model.objects.all()
     
+    def get_user_permissions(self):
+        content_type = ContentType.objects.get_for_model(self.model)
+        all_permissions = Permission.objects.filter(content_type=content_type)
+        permissions = []
+        for p in all_permissions:
+            if self.request.user.has_perm("core." + p.codename):
+                permissions.append(p.codename)
+        return permissions
+
     def get_serializer_class(self):
         GeneralSerializer.Meta.model = self.model
+        permissions = self.get_user_permissions()
+        model_fields = [f.name for f in self.model._meta.get_fields()]
+        fields = []
+        for p in permissions:
+            if p in model_fields:
+                fields.append(p)
+        GeneralSerializer.Meta.fields = fields
         return GeneralSerializer
